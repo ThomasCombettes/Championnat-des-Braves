eb35dd0e94fe85a7a7d81f216f09f1a1df099434
thomascombettes <thomascombettes@gmail.com>
2023-09-21 12:33:58 +0200

    jeu

--- championnat-des-braves/src/routes/jeux/jeux-a-boire/mon-glaive-mon-foie/+page.svelte
+++ championnat-des-braves/src/routes/jeux/jeux-a-boire/mon-glaive-mon-foie/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   let roles = "ecuyer";
   import { TabGroup, Tab, TabAnchor } from "@skeletonlabs/skeleton";
+  import { Footer } from "flowbite-svelte";
 
+  import ecu from "$lib/ecuyer.png"
   let tabSet = 0;
 </script>
 
@@ -17,17 +19,34 @@
 
   <svelte:fragment slot="panel">
     {#if tabSet === 0}
-      <div>
+      <div id="cards">
+        <div class="card">
+          <header class="card-header">(header)</header>
+          <section class="p-4">
+            ( <h2 class="h2">
+              LANCEMENT DE DES
+              <a href="https://github.com/EmPierrain/MGMF/">github</a>
+            </h2>
+            )
+          </section>
+          <footer class="card-footer">(footer)</footer>
+        </div>
+
         {#if (roles = "ecuyer")}
-          <h1 class="h1">③① Ecuyer</h1>
-          <p class="p">
-            S'il est Dieu ou héro ou qu'uncun héros n'existe, le rôle n'est pas
-            attribué et la personne boit.<br />
+          <div class="card">
+            <header class="card-header"><img src={ecu} alt='ecuyer'></header>
+            <section class="p-4">
+              <p class="p">
+                S'il est Dieu ou héro ou qu'uncun héros n'existe, le rôle n'est
+                pas attribué et la personne boit.<br />
 
-            L'écuyer boit à chaque fois que le héro boit.<br />
+                L'écuyer boit à chaque fois que le héro boit.<br />
 
-            Il perd son rôle si le héros perd son rôle.
-          </p>
+                Il perd son rôle si le héros perd son rôle.
+              </p>
+            </section>
+            <footer class="card-footer"><h1 class="h1">③① Ecuyer</h1></footer>
+          </div>
         {/if}
       </div>
     {:else if tabSet === 1}
@@ -37,4 +56,9 @@
     {/if}
   </svelte:fragment>
 </TabGroup>
-<p>mon glaive mon foie</p>
+
+<style>
+  #cards {
+    display: flex;
+  }
+</style>
